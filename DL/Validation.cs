55cbd55da8ef3db39e22da3a08f2250cdddc92c8
Oldmanpitsa54 <bublik12367@gmail.com>
2020-09-06 01:28:03 +0300

    Validation method realization

---
+++ DL/Validation.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DL
+{
+    class Validation
+    {
+        public static void Decimal_Digits_check(string input, out decimal output)
+        {
+            if (!decimal.TryParse(input, out output))
+            {
+                while (true)
+                {
+                    Console.WriteLine("Invalid value (not a digit). Try again.");
+                    input = Console.ReadLine();
+                    if (decimal.TryParse(input, out output))
+                    {
+                        output = decimal.Parse(input); //
+                        break;
+                    }
+                }
+            }
+            else
+            {
+                output = decimal.Parse(input);
+            }
+        }
+    }
+}
