1c71b017cd4a02323eec748a3dfe074e1b042e5f
Oldmanpitsa54 <bublik12367@gmail.com>
2020-09-06 16:15:22 +0300

    Furniture class realization

---
+++ DL/Furniture.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DL
+{
+    public class Furniture
+    {
+        public Furniture(string type, string colour, double size, double weight, string manufacturer)
+        {
+            FurnitureType = type;
+            Colour = colour;
+            Size = size;
+            Weight = weight;
+            Manufacturer = manufacturer;
+        }
+        public string FurnitureType { get; set; }
+        public string Colour { get; set;}
+        public double Size { get; set; }
+        public double Weight { get; set; }
+        public string Manufacturer { get; set; }
+    }
+    public class Material
+    {
+        public Material(double size, double cost)
+        {
+            Size = size;
+            Cost = cost;
+        }
+        public double Size { get; set; }
+        public double Cost { get; set; }
+    }
+}
