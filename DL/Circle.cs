9b60a74f51b899c6a458f8d503a987fe92cc57d2
Oldmanpitsa54 <bublik12367@gmail.com>
2020-09-06 13:13:59 +0300

    Cirecle class realization

---
+++ DL/Circle.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+ 
+namespace DL
+{
+    
+   public class Circle
+    {
+        public const double pi = 3.14;
+        public Circle(Point center, double radius)
+        {
+            Center = center;
+            Radius = radius;
+            Length = Radius * 2 * pi;
+            Area = Math.Pow(Radius, 2) * pi;
+        }
+        public Point Center { get; set; }
+        public double Length { get; set; }
+        public double Radius { get; set; }
+        public double Area { get; set; }
+            
+        public static bool Compare(Circle c1, Circle c2)
+        {
+            if (c1.Area == c2.Area)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+
+            }
+        }
+    }
+}
